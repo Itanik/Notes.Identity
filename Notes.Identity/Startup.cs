97941714bee445df52ef3ef456f2460056562f9a
Nikita Ulyantsev <yan.xhrome@gmail.com>
2022-04-13 01:46:37 +0300

    Configure entity framework for credentials storage.

--- Notes.Identity/Startup.cs
+++ Notes.Identity/Startup.cs
@@ -1,28 +1,58 @@
-using IdentityServer4.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Notes.Identity.Data;
+using Notes.Identity.Data.Models;
 
 namespace Notes.Identity
 {
     public class Startup
     {
+        public IConfiguration AppConfiguration { get; }
+
+        public Startup(IConfiguration configuration) => AppConfiguration = configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = AppConfiguration.GetValue<string>("DbConnection");
+
+            services.AddDbContext<AuthDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+
+            services.AddIdentity<AppUserConfiguration, IdentityRole>(config =>
+            {
+                config.Password.RequiredLength = 4;
+                config.Password.RequireDigit = false;
+                config.Password.RequireNonAlphanumeric = false;
+                config.Password.RequireUppercase = false;
+            })
+                .AddEntityFrameworkStores<AuthDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddIdentityServer()
+                .AddAspNetIdentity<AppUser>()
                 .AddInMemoryApiResources(Configuration.ApiResources)
                 .AddInMemoryIdentityResources(Configuration.IdentityResources)
                 .AddInMemoryApiScopes(Configuration.ApiScopes)
                 .AddInMemoryClients(Configuration.Clients)
                 .AddDeveloperSigningCredential();
+
+            // настройка куки для хранения токена
+            services.ConfigureApplicationCookie(config =>
+            {
+                config.Cookie.Name = "Notes.Identity.Cookie";
+                config.LoginPath = "/Auth/Login";
+                config.LogoutPath = "/Auth/Logout";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
