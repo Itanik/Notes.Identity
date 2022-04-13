418fa62936b2d3b97b609862499cf3791731ce5a
Nikita Ulyantsev <yan.xhrome@gmail.com>
2022-04-13 14:19:56 +0300

    Add register method.

--- Notes.Identity/Controllers/AuthController.cs
+++ Notes.Identity/Controllers/AuthController.cs
@@ -51,5 +51,36 @@ namespace Notes.Identity.Controllers
             ModelState.AddModelError(string.Empty, "Login error");
             return View(viewModel);
         }
+
+        [HttpGet]
+        public IActionResult Register(string returnUrl)
+        {
+            var viewModel = new RegisterViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel)
+        {
+            if (!ModelState.IsValid) return View(viewModel);
+
+            var user = new AppUser
+            {
+                UserName = viewModel.UserName,
+            };
+
+            var result = await _userManager.CreateAsync(user, viewModel.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Redirect(viewModel.ReturnUrl);
+            }
+            ModelState.AddModelError(string.Empty, "Register error");
+            return View(viewModel);
+        }
     }
 }
