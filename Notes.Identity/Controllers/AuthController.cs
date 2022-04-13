34b8792ed2216bd3408615eb3f7e6ca93df82cf8
Nikita Ulyantsev <yan.xhrome@gmail.com>
2022-04-13 13:51:27 +0300

    Add login validation.

--- Notes.Identity/Controllers/AuthController.cs
+++ Notes.Identity/Controllers/AuthController.cs
@@ -1,10 +1,24 @@
+using System.Threading.Tasks;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Notes.Identity.Data.Models;
 using Notes.Identity.Models;
 
 namespace Notes.Identity.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IIdentityServerInteractionService _interactionService;
+
+        public AuthController(SignInManager<AppUser> signInManager,
+            UserManager<AppUser> userManager,
+            IIdentityServerInteractionService interactionService) =>
+            (_signInManager, _userManager, _interactionService) =
+            (signInManager, userManager, interactionService);
+
         [HttpGet]
         public IActionResult Login(string returnUrl)
         {
@@ -16,8 +30,25 @@ namespace Notes.Identity.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel viewModel)
+        public async Task<IActionResult> LoginAsync(LoginViewModel viewModel)
         {
+            if (!ModelState.IsValid) return View(viewModel);
+
+            var user = await _signInManager.PasswordSignInAsync(viewModel.UserName,
+                viewModel.Password, false, false);
+
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "User not found");
+                return View(viewModel);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(viewModel.UserName, viewModel.Password, false, false);
+
+            if (result.Succeeded)
+                return Redirect(viewModel.ReturnUrl);
+
+            ModelState.AddModelError(string.Empty, "Login error");
             return View(viewModel);
         }
     }
